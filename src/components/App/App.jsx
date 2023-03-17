eb2e6eab3dfc9e87480df92f5e71dc56aae8cb0d
Valeriia Frolova <valeriiats1989@gmail.com>
2023-03-17 18:40:36 +0200

    add changes in files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+// import { Route, Routes } from 'react-router-dom';
+// import Home from '../../pages/home';
+// import Movies from '../../pages/movies';
+// import { Layout } from 'Layout/Layout';
+export const App = () => {
+  return (
+    <div>
+      {/* <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<div>Movie Details</div>} />
+        </Route>
+      </Routes> */}
+    </div>
+  );
+};
